a58f8814bb75937f8e6cb1415e267a9cb2593ae9
jay-87 <nguyen.nhu.lan@moneyforward.vn>
2024-05-17 09:00:24 +0700

    fix condition

--- cluster/local.tf
+++ cluster/local.tf
@@ -1,4 +1,4 @@
 locals {
   bucket_arn_list      = var.bucket_arn_list
-  sub_bucket_arn_list  = [for arn in var.bucket_arn_list : "${arn}/*"]
+  sub_bucket_arn_list  = length(var.bucket_arn_list) > 0 ? [for arn in var.bucket_arn_list : "${arn}/*"] : ["arn:aws:s3:::xxxx"]
 }
--- service_load_balancing/local.tf
+++ service_load_balancing/local.tf
@@ -2,5 +2,5 @@ locals {
   enable_service_discovery       = var.enable_service_discovery
   service_discovery_registry_arn = var.enable_service_discovery && var.service_discovery_registry_arn != "" ? var.service_discovery_registry_arn : null
   bucket_arn_list                = var.bucket_arn_list
-  sub_bucket_arn_list            = [for arn in var.bucket_arn_list : "${arn}/*"]
+  sub_bucket_arn_list            = length(var.bucket_arn_list) > 0 ? [for arn in var.bucket_arn_list : "${arn}/*"] : ["arn:aws:s3:::xxxx"]
 }
